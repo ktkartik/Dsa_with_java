b47386e237aad07625a3e3dcf6443d067c328792
ktkartik <kt089317@gmail.com>
2025-07-28 22:33:23 +0530

    weight

---
+++ weight/Weight.java
@@ -0,0 +1,28 @@
+package weight;
+import java.util.Scanner;
+
+class Weight{
+    static int profitt(double  num){
+        double weighting=num*1000;
+        double profitperg=0.075;
+        double profit=weighting* profitperg;
+        return (int)profit;
+    }
+    static  int returnweight(double num){
+        int Profitweight=profitt( num);
+        double weighting=num*1000;
+        double returnw=weighting-Profitweight;
+        //returnw=returnw/1000;
+        System.out.println("Return weight => "+ returnw);
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        Scanner sc =new Scanner(System.in);
+        System.out.print("enter the weight in kg => ");
+        double weight=sc.nextFloat();
+        System.out.println("your profit weight in g => "+profitt(weight));
+        returnweight(weight);
+        
+    }
+}
